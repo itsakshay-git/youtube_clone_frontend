92328923950333eeab27e0fdabfdbfb57b1e2467
itsakshay-git <110621476+itsakshay-git@users.noreply.github.com>
2025-05-13 22:24:57 +0530

    add fallback in editpage

--- src/pages/EditChannel.jsx
+++ src/pages/EditChannel.jsx
@@ -71,7 +71,14 @@ function EditChannel() {
   
   
 
-  if (!channel) return <div className="p-4">Loading...</div>;
+  if (!channel){
+      return (
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="w-[50%] h-[50%] shadow-2xl rounded-3xl flex justify-center items-center text-black">
+        <p>Loading...</p>
+      </div>
+    </div>
+)};
 
   return (
   <div className="p-6">
